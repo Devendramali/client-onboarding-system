6b55a5745f9fcdeccfad50563066dd7ee1faa722
Devendramali <devendramali01234@gmail.com>
2026-08-12 14:48:08 +0530

    new

--- app/[lang]/(dashboard)/(invoice)/invoice-list/invoice-list-table/index.jsx
+++ app/[lang]/(dashboard)/(invoice)/invoice-list/invoice-list-table/index.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
-import { columns } from "./components/columns";
-import { DataTable } from "./components/data-table";
+import { columns } from "@//components/columns";
+import { DataTable } from "@//components/data-table";
 import { data } from "./data";
 
 export default function InvoiceListTable() {
