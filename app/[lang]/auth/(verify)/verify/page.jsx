f8ddf17a62e77fee0f22d1975f425615f0f64736
Devendramali <devendramali01234@gmail.com>
2026-08-14 18:00:37 +0530

    new chnages

--- app/[lang]/auth/(verify)/verify/page.jsx
+++ app/[lang]/auth/(verify)/verify/page.jsx
@@ -18,42 +18,45 @@ const VerifyPage = () => {
     <Fragment>
       <div className="min-h-screen bg-card  flex items-center  overflow-hidden w-full">
         <div className="lg-inner-column flex w-full flex-wrap justify-center overflow-y-auto">
-          <div
-            className="basis-1/2 bg-[#1b776f] w-full relative hidden xl:flex justify-center items-center bg-linear-to-br
-          from-[#1b776f]-600 via-[#1b776f]-400 to-[#1b776f]-600"
-          >
-            <Image
-              src={background}
-              alt="image"
-              className="absolute top-0 left-0 w-full h-full"
-            />
-            <div className="relative z-10 backdrop-blur-sm bg-primary-foreground/40 py-[84px] pl-[50px] pr-[136px] rounded">
-              <div>
-                <Button
-                  className="bg-transparent hover:bg-transparent h-fit w-fit p-0"
-                  onClick={() => setOpenVideo(true)}
-                >
-                  <Icon
-                    icon="heroicons:play-solid"
-                    className="text-primary-foreground h-[78px] w-[78px] -ml-2"
-                  />
-                </Button>
-
-                <div className="text-6xl leading-[72px] font-semibold mt-2.5">
-                  <span className="text-slate-700">
-                    Unlock <br />
-                    Your Project
-                  </span>{" "}
-                  <br />
-                  <span className="text-slate-700">Performance</span>
-                </div>
-                <div className="mt-8 text-default-900 dark:text-slate-200  text-2xl font-medium">
-                  You will never know everything. <br />
-                  But you will know more...
-                </div>
-              </div>
-            </div>
-          </div>
+          <div className="basis-1/2 bg-[#1b776f] w-full relative hidden xl:flex justify-center items-center bg-linear-to-br
+                                    from-[#1b776f]-600 via-[#1b776f]-400 to-[#1b776f]-600"
+                                    >
+                                      <Image
+                                        src={background}
+                                        alt="image"
+                                        className="absolute top-0 left-0 w-full h-full"
+                                      />
+                                      <div className="relative z-10 backdrop-blur-sm bg-primary-foreground/40 py-14 px-16 2xl:py-[84px] 2xl:pl-[50px] 2xl:pr-[136px] rounded max-w-[640px]">
+                                        <div>
+                                          <Button
+                                            className="bg-transparent hover:bg-transparent h-fit w-fit p-0"
+                                            onClick={() => setOpenVideo(true)}
+                                          >
+                                            <Icon
+                                              icon="heroicons:play-solid"
+                                              className="text-primary-foreground h-[78px] w-[78px] -ml-2"
+                                            />
+                                          </Button>
+                          
+                                          {/* <div className="text-6xl leading-[72px] font-semibold mt-2.5">
+                                            <span className="text-slate-700">
+                                              Unlock <br />
+                                              Your Project
+                                            </span>{" "}
+                                            <br />
+                                            <span className="text-slate-700">Performance</span>
+                                          </div> */}
+                                          <div className="text-4xl leading-[50px] 2xl:text-6xl 2xl:leading-[72px] font-semibold mt-2.5">
+                                            {/* <span </span> */}
+                                            <span className="text-default-600 dark:text-default-300 ">Unlock <br/>Your Project <br /></span>
+                                            <span className="text-default-900 dark:text-default-50">Performance</span></div>
+                                          <div className="mt-8 text-default-900 dark:text-slate-200  text-2xl font-medium">
+                                            You will never know everything. <br />
+                                            But you will know more...
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
 
           <div className=" min-h-screen basis-full md:basis-1/2 w-full px-4 py-5 flex justify-center">
             <div className="lg:w-[480px]">
