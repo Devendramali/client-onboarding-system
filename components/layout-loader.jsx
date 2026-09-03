4a139cbe976d797667c7cfcd4aea898bb693e3b5
Devendramali <devendramali01234@gmail.com>
2026-09-03 18:45:51 +0530

    new

--- components/layout-loader.jsx
+++ components/layout-loader.jsx
@@ -2,10 +2,16 @@
 import React from "react";
 import { SiteLogo } from "@/components/svg";
 import { Loader2 } from "lucide-react";
+import Image from "next/image";
 const LayoutLoader = () => {
   return (
     <div className=" h-screen flex items-center justify-center flex-col space-y-2">
-      <SiteLogo className=" h-10 w-10 text-primary" />
+       <Image
+                src="/images/all-img/logo.png"
+                alt="DigiHost Logo"
+                width={156}
+                height={56}
+              />
       <span className=" inline-flex gap-1">
         <Loader2 className="mr-2 h-4 w-4 animate-spin" />
         Loading...
--- components/partials/footer/index.jsx
+++ components/partials/footer/index.jsx
@@ -83,18 +83,9 @@ const FooterContent = () => {
   return (
     <div className="block md:flex md:justify-between text-muted-foreground">
       <p className="sm:mb-0 text-xs md:text-sm">
-        COPYRIGHT © {new Date().getFullYear()} DashTail All rights Reserved
+        Copyright © {new Date().getFullYear()} Client Onboarding System. All Rights Reserved. Powered By : <a href="https://www.digihost.in/" target="_blank">DIIGIIHOST</a>
       </p>
-      <p className="mb-0 text-xs md:text-sm">
-        Hand-crafted & Made by{" "}
-        <a
-          className="text-primary"
-          target="__blank"
-          href="https://codeshaper.net"
-        >
-          Codeshaper
-        </a>
-      </p>
+      
     </div>
   );
 };
