44402d2c78e7ba870cc46cf769477567b744265b
Devendramali <devendramali01234@gmail.com>
2026-09-15 13:34:55 +0530

    new changes

--- app/[lang]/(dashboard)/(home)/billing-account-management/add/page.jsx
+++ app/[lang]/(dashboard)/(home)/billing-account-management/add/page.jsx
@@ -993,8 +993,28 @@ export default function Page() {
 
 
                 </div>
-                <div className="border border-[#D9D9D9] rounded-[16px] p-[16px] mt-[16px]">
-                  <h4 className="text-[20px] text-[#1A766D] font-[500] mb-[24px]">Contact</h4>
+              <div className="border border-[#D9D9D9] rounded-[16px] p-[16px] mt-[16px]">
+                  <h4 className="text-[20px] text-[#1A766D] font-[500] mb-[24px]">Associated Contact</h4>
+                  <div className="grid grid-cols-2 gap-[24px]">
+                    <div className="lg:col-span-1 col-span-2">
+                      <h4 className="text-[16px] text-[#757575]">Contact Name:</h4>
+                      <h3 className="text-[#1E1E1E] text-[20px]">jhone doe</h3>
+                    </div>
+                    <div className="lg:col-span-1 col-span-2">
+                      <h4 className="text-[16px] text-[#757575]">Contact Email Address:</h4>
+                      <h3 className="text-[#1E1E1E] text-[20px]">abc@gmail.com</h3>
+                    </div>
+                    <div className="lg:col-span-1 col-span-2">
+                      <h4 className="text-[16px] text-[#757575]">Contact Phone Number:</h4>
+                      <h3 className="text-[#1E1E1E] text-[20px]">Mumbai</h3>
+                    </div>
+                     <div className="lg:col-span-1 col-span-2">
+                                <h4 className="text-[16px] text-[#757575]">Role</h4>
+                                <h3 className="text-[#1A766D] bg-[#E8FFFB] w-fit rounded-[100px] px-[16px] py-[8px] text-[16px]">Admin</h3>
+                              </div>  
+                   
+                  </div>
+                  <hr className="border-[#D9D9D9] border border-t-0 my-[16px]"/>
                   <div className="grid grid-cols-2 gap-[24px]">
                     <div className="lg:col-span-1 col-span-2">
                       <h4 className="text-[16px] text-[#757575]">Contact Name:</h4>
